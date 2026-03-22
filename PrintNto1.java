458a4613cc09b5597c8782452685a1fd89eba2c7
varshamgowda34-blip <varshamgowda34@gmail.com>
2026-03-22 23:26:56 +0530

    Add files via upload

---
+++ PrintNto1.java
@@ -0,0 +1,13 @@
+package assignment4.module9;
+
+public class PrintNto1 {
+    static void print(int n) {
+        if (n == 0) return;
+        System.out.println(n);
+        print(n - 1);
+    }
+
+    public static void main(String[] args) {
+        print(5);
+    }
+}
---
+++ ReplaceSpaces.java
@@ -0,0 +1,8 @@
+package assignment4.module8;
+
+public class ReplaceSpaces {
+    public static void main(String[] args) {
+        String str = "Java is fun";
+        System.out.println(str.replace(" ", "%20"));
+    }
+}
